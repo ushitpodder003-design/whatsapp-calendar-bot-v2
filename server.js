b43b43bfa91774493a37e376d8e62fd957273caf
Ushit Podder <ushit.podder@reelo.io>
2026-03-12 17:43:07 +0530

    Add form-encoded middleware for Twilio webhook

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authHandler = require('./src/handlers/auth');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     console.log(`📨 ${req.method} ${req.path}`);
